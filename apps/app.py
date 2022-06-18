096edca7414c2b721d4f03025c54de3f1dccec64
mingdebaba <51104897+mingdebaba@users.noreply.github.com>
2022-06-18 21:41:52 +0900

    login logout

--- apps/app.py
+++ apps/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 from pathlib import Path
 from flask_migrate import Migrate
 from flask_sqlalchemy import  SQLAlchemy
@@ -8,6 +9,13 @@ from apps.config import config
 
 db= SQLAlchemy()
 csrf = CSRFProtect()
+# LoginManagerをインスタンス化する
+login_manager = LoginManager()
+# login_view属性に未ログイン時にリダイレクトするエンドポイントを指定する
+login_manager.login_view = "auth.signup"
+# login_message属性にログイン後に表示するメッセージを指定する
+# ここでは何も表示しないよう空を指定する
+login_manager.login_message = ""
 
 # create_app関数を作成する
 def create_app(config_key):
@@ -20,9 +28,17 @@ def create_app(config_key):
 
     Migrate(app,db)
     csrf.init_app(app)
-    
+     # login_managerをアプリケーションと連携する
+    login_manager.init_app(app)
+
     from apps.crud import views as crud_views
 
     app.register_blueprint(crud_views.crud,url_prefix="/crud")
+
+    # これから作成するauthパッケージからviewsをimportする
+    from apps.auth import views as auth_views
+
+     # register_blueprintを使いviewsのauthをアプリへ登録する
+    app.register_blueprint(auth_views.auth, url_prefix="/auth")
 
     return app
--- apps/crud/views.py
+++ apps/crud/views.py
@@ -2,6 +2,7 @@ from apps.app import db
 from flask import Blueprint,render_template,redirect,url_for
 from apps.crud.models import User
 from apps.crud.forms import UserForm
+from flask_login import login_required
 #Blueprint de crud アプリを生成する
 crud = Blueprint(
     "crud",
@@ -10,22 +11,27 @@ crud = Blueprint(
     static_folder="static",
 )
 
+
 # indexエンドポイントを作成しindex.htmlを返す
 @crud.route("/")
+@login_required
 def index():
     return render_template("crud/index.html")
 
 @crud.route("/sql")
+@login_required
 def sql():
     db.session.query(User).all()
     return "コンソールログを確認してください"
 
-@crud.route("/users")
+@crud.route("/users",methods=["GET", "POST"])
+@login_required
 def users():
     users = User.query.all()
     return render_template("crud/index.html",users=users)
 
 @crud.route("/users/new",methods=["GET","POST"])
+@login_required
 def create_user():
     form = UserForm()
 
@@ -38,10 +44,11 @@ def create_user():
         db.session.add(user)
         db.session.commit()
 
-        return redirect(url_for("crud.user"))
+        return redirect(url_for("crud.users"))
     return render_template("crud/create.html",form=form)
 
 @crud.route("/users/<user_id>", methods=["GET", "POST"])
+@login_required
 def edit_user(user_id):
     form = UserForm()
 
@@ -60,9 +67,12 @@ def edit_user(user_id):
     return render_template("crud/edit.html",user=user,form=form)
 
 @crud.route("/users/<user_id>/delete", methods=["POST"])
-
+@login_required
 def delete_user(user_id):
     user = User.query.filter_by(id=user_id).first()
     db.session.delete(user)
     db.session.commit()
     return redirect(url_for("crud.users"))
+
+
+
