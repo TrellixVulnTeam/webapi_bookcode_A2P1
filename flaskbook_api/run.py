aa20a3be4b2abdf4f4f102af7f30664eba1471d0
mingdebaba <51104897+mingdebaba@users.noreply.github.com>
2022-07-04 00:30:12 +0900

    データ準備、前処理、後処理

--- flaskbook_api/run.py
+++ flaskbook_api/run.py
@@ -1,4 +1,13 @@
 import os
-from flaskbook_api.api import create_app
-config = os.environ.get("CONFIG","local")
-app= create_app(config)
+
+from flask import Flask
+from flaskbook_api.api import api
+from flaskbook_api.api.config import config
+
+
+
+config_name = os.environ.get("CONFIG","local")
+app=Flask(__name__)
+app.config.from_object(config[config_name])
+# blueprintをアプリケーションに登録
+app.register_blueprint(api)
