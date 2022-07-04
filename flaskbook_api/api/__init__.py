dee866f817a4e3c6136bee163ed1d71ee8dfe468
mingdebaba <51104897+mingdebaba@users.noreply.github.com>
2022-07-04 21:20:48 +0900

    ルーティング

--- flaskbook_api/api/__init__.py
+++ flaskbook_api/api/__init__.py
@@ -2,3 +2,11 @@ from flask import Blueprint,jsonify,request
 
 from flaskbook_api.api import calculation
 api=Blueprint("api",__name__)
+
+@api.get("/")
+def index():
+    return jsonify({"column":"value"}),2018
+
+@api.post("/detect")
+def detection():
+    return calculation.detection(request)
